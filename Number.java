c93a359d3ab2a4fc5a58923b51fda301384f3297
jyothipasumarthi <jyothi1250@sasi.ac.in>
2022-07-22 21:27:33 +0530

    add4

---
+++ Number.java
@@ -0,0 +1,15 @@
+public class Number{
+	public static void main(String[] args){
+		for(int x=1;x<=10;x++){
+			System.out.println(+x);
+		}
+		/*while(x<=10){
+			System.out.println(+x);
+			x++;
+		}
+		do{
+			System.out.println(+x);
+			x++;
+		}while(x<=10);*/
+	}
+}
