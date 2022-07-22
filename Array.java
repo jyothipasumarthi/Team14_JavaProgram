fe61124609ee79b8baacd1a09fc89bb5242288ae
jyothipasumarthi <jyothi1250@sasi.ac.in>
2022-07-22 21:32:31 +0530

    add5

---
+++ Array.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;;
+public class Array{
+	public static void main(String[] args){
+		Scanner s=new Scanner(String [] args);
+		int a[]=new Int[5];
+		System.out.println("Enter 5 values: ");
+		for(int i=0;i<=4;i++){
+			System.out.println(i);
+		}
+	}
+}
