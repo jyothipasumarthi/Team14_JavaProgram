52bebbaef0455192686742b9d5a86218808e6119
jyothipasumarthi <jyothi1250@sasi.ac.in>
2022-07-22 21:09:57 +0530

    add2

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+import java.uitl.Scanner;
+public class DataTypes
+{
+    public static void main(String args[])
+    {
+	  Scanner sc=new Scanner(System.in);
+        System.out.println("byte value : ");
+	  byte b=sc.next Byte();
+        System.out.println("float value : ");
+	  float f=sc.next Float();
+	  System.out.println("byte default value: ");
+	  System.out.println("float default value: ");
+
+    }
+}
